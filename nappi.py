fba56625176b9bfa107c5fe0bf18d7a7fb684c28
root <root@janhopi.(none)>
2016-05-30 19:34:10 +0300

    jou

--- nappi.py
+++ nappi.py
@@ -1,3 +1,4 @@
+import httplib
 import RPi.GPIO as GPIO
 from time import sleep
 
@@ -6,10 +7,17 @@ GPIO.setup(4,GPIO.IN)
 
 pressed = False
 
+def buttonPressed():
+	conn = httplib.HTTPConnection("192.168.180.23:1337")
+	conn.request("GET", "/api?con=7&cpu=2")
+	return True
+
 while True:
 	if(GPIO.input(4) == True):
 		pressed = False	
 	elif(GPIO.input(4) == False and not pressed):
 		pressed = True
 		print 'moi'
+		buttonPressed()
 	sleep(0.05)
+
