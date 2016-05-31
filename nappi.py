2a28ffe650fa960c04bd0f32199a5fd9958be4c5
koodinikkari <koodinikkari@users.noreply.github.com>
2016-05-31 20:17:04 +0300

    Update nappi.py

--- nappi.py
+++ nappi.py
@@ -10,7 +10,19 @@ pressed = False
 def buttonPressed():
 	conn = httplib.HTTPConnection("taisto.ths.dom:1337")
 	conn.request("GET", "/api?con=7&cpu=2")
-	return True
+
+def wakeuptykkikone():
+	conn = httplib.HTTPConnection("wakeup.ths.dom")
+	conn.request("GET", "/api/?machine_id=1")
+	
+def wakeupnstreamkone():
+	conn = httplib.HTTPConnection("wakeup.ths.dom")
+	conn.request("GET", "/api/?machine_id=2")
+	
+def wakeupnstreamkone():
+	conn = httplib.HTTPConnection("wakeup.ths.dom")
+	conn.request("GET", "/api/?machine_id=7")
+	
 
 while True:
 	if(GPIO.input(4) == True):
